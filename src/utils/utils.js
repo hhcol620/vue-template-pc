e6c51428a0f4ba93e221130e89e66c0b6a46a241
hhcol <2838967868@qq.com>
2021-01-17 15:24:04 +0800

    处理完成防抖

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 /**
- * @params : fn需要防抖的函数 delay延迟时间 immediately是否需要立即执行  如果被连续调用时间间隔一直小于delay ms的话,只有最后一次会被调用到fn
+ * @params : fn需要防抖的函数 delay延迟时间 immediately是否需要立即执行
  * @return :
  * @example : 付款按钮 点赞按钮 这种容易被人连续点击的按钮
  * @description : 防抖 _debounce  防止用户快速连续点击造成短时间内发出多个重复的请求,给服务器造成压力
@@ -8,18 +8,27 @@
  */
 
 function _debounce(fn, delay = 300, immediately = true) {
-    let timer = null;
+    let timer = null,
+        result,
+        timeStamp = 0;
     return function(...args) {
+        timeStamp = Date.now();
         if (timer) clearTimeout(timer);
-        if (!timer && immediately) fn.apply(this, args);
+        if (!timer && immediately) {
+            result = fn.apply(this, args);
+        }
         timer = setTimeout(() => {
-            fn.apply(this, args);
+            timer = null;
+            if (!immediately) {
+                result = fn.apply(this, args);
+            }
         }, delay);
+        return result;
     };
 }
 
 /**
- * @params : fn需要节流的函数 delay控制相邻两次的时间间隔最少是 delay ms   节流参数配置options {}
+ * @params : fn需要节流的函数 delay控制相邻两次的时间间隔最少是 delay ms   节流参数配置options {} 传入{leading:false} 忽略开始函数的调用 传入{trailing:false} 忽略结尾函数调用 两者  不能共存
  * @return :
  * @example : 使用场景 比如滚动事件处理函数
  * @description : 节流 _throttle  在需要连续触发的事件进行稀释  减少cpu 或者是内存的占用
@@ -27,14 +36,13 @@ function _debounce(fn, delay = 300, immediately = true) {
  * @author : huanghe
  */
 
-function _throttle(fn, delay = 300) {
+function _throttle(fn, delay = 300, options = { leading: false }) {
     //节流
     let lastTime = 0;
     return function(...args) {
         let nowTime = new Date().getTime();
         if (nowTime - lastTime >= delay) {
             fn.apply(this, args);
-            i;
             lastTime = nowTime;
         }
     };
