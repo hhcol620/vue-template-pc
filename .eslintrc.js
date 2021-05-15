5f67b6c85147bf19ebeda5904d634bdd47529313
hhcol <2838967868@qq.com>
2021-05-15 22:44:10 +0800

    add eslint desc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
     },
     extends: ['eslint:recommended', 'plugin:vue/essential'],
     parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module'
+        ecmaVersion: 12, // 支持es6语法，但并不意味着同时支持新的 ES6 全局变量或类型（比如 Set 等新类型）
+        sourceType: 'module' // 指定来源的类型，"script" (默认) 或 "module"（如果你的代码是 ECMAScript 模块)
     },
     plugins: ['vue'],
     rules: {
